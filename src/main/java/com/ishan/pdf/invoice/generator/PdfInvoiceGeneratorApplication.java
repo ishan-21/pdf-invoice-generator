791ddf4f117a50c44797140eab9628563ce8083f
ishan <raiishan0@gmail.com>
2024-10-19 17:16:07 +0530

    springboot project initialized

---
+++ src/main/java/com/ishan/pdf/invoice/generator/PdfInvoiceGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.ishan.pdf.invoice.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfInvoiceGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfInvoiceGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ishan/pdf/invoice/generator/PdfInvoiceGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishan.pdf.invoice.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfInvoiceGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
